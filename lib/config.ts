dacaacaf270ce7c9bde6d67faae1183438df1d53
hawkvision09 <hawkvision09@gmail.com>
2026-01-23 17:04:47 +0530

    production login error fix try02

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,7 @@
 export const SERVICE_ACCOUNT_EMAIL = process.env.MASTER_SERVICE_ACCOUNT_EMAIL;
 export const SPREADSHEET_ID = process.env.MASTER_SPREADSHEET_ID;
-export const SERVICE_ACCOUNT_KEY = process.env.MASTER_SERVICE_ACCOUNT_PRIVATE_KEY
-  ? process.env.MASTER_SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, '\n')
+
+const privateKey = process.env.MASTER_SERVICE_ACCOUNT_PRIVATE_KEY;
+export const SERVICE_ACCOUNT_KEY = privateKey
+  ? privateKey.replace(/^"|"$/g, '').replace(/\\n/g, '\n')
   : undefined;
